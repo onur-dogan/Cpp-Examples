b090146aac81fd8cd54f7f80e0c2a1dd1a07bf2f
onur-dogan <aindres@hotmail.com>
2024-11-15 01:52:32 +0300

    Create static variables in class to manage global properties

--- ClassExamples/Birthdate/src/main.cpp
+++ ClassExamples/Birthdate/src/main.cpp
@@ -7,16 +7,25 @@ int main()
     Date birthDate(1961, 4, 12);
     Date joinDate(1990, 6, 24);
 
+    // Register first person
     Person firstPerson("Test", "Person", "Male", birthDate, joinDate);
 
     Date birthDate2(1970, 12, 3);
     Date joinDate2(1992, 1, 6);
     Person secondPerson;
+    // Register second person
     secondPerson.setInformations("Test2", "Person2", 2, birthDate2, joinDate2);
+
+    // Show the person list
+    std::cout << "\n"
+              << std::setw(40) << std::right << "*** PERSON LIST ***"
+              << "\nPerson Count which is registered to the system: \t" << firstPerson.getPersonsCount()
+              << std::endl;
 
     firstPerson.showInformations();
     secondPerson.showInformations();
 
+    // Update first person's information by using a common function
     std::string newFirstName, newLastName;
     std::cout << "Enter first and last name to change the first person's full name" << std::endl;
     std::cin >> newFirstName >> newLastName;
@@ -24,6 +33,7 @@ int main()
     firstPerson.setFullName(newFirstName, newLastName);
     firstPerson.showInformations();
 
+    // Update second person's information by using a friend function 
     std::string newName;
     std::cout << "Enter a name to change the second person's name" << std::endl;
     std::cin >> newName;
