542167c815aa056673e377cc6130bc83f6db9f6c
onur-dogan <aindres@hotmail.com>
2024-11-25 01:49:46 +0300

    Define functions and descriptions as a list and improve code quality

---
+++ GeneralExamples/src/constants.hpp
@@ -0,0 +1,43 @@
+// CONSTANTS_HPP
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#include <iomanip>
+
+constexpr std::string_view Constants[] =
+    {
+        // calculateWithNumbers
+        "Make some calculations between two integers",
+        // compareNumbers
+        "Compare the numbers",
+        // drawShapes
+        "Draw different shapes",
+        // showASCIIEquivalent
+        "Show ASCII equivalent of any character",
+        // calculateBMI
+        "Calculate BMI(Body Mass Index) and show the result",
+        // calculateVehicleExpenses
+        "Calculate daily vehicle drive expenses",
+        // loopComparator
+        "Compare the for, while, and do-while loops performances",
+        // printNumbersInOrder
+        "Print the (even or odd) numbers in order",
+        // calculateGrades
+        "Calculate average grades by using grade points and displays the final class grade information",
+        // generateRandomNumbers
+        "Generate random numbers",
+        // overloadMultiplyCalculation
+        "Multiply the numbers for different parameter data types via overloaded functions",
+        // sortAndSearchInArray
+        "General basic sorting and searching in array (used with array library)",
+        // generalVectorFeatures
+        "Create vector variables, and test and review the vector features",
+        // findArrayLengthBySize
+        "Find array length by calculating the byte size of the variables",
+        // displayBytesByDataType
+        "Displays bytes for each data type",
+        // stringCharArrayUsages
+        "Some examples about using string/char array"
+};
+
+#endif
